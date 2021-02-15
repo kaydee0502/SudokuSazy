89df5cb6f4ad1a5dbb79e94653ec17a07f0f60b9
kaydee0502 <kshitijdhama513@gmail.com>
2021-02-15 20:29:56 +0530

    e1

--- model/vcopy.py
+++ model/vcopy.py
@@ -26,12 +26,41 @@ class Model:
         #plt.imshow(sud)
         (thresh, sud) = cv2.threshold(sud, 150, 255, cv2.THRESH_BINARY_INV)
 
+        cells = []
+
         for r in range(0,252,28):
             for c in range(0,252,28):
                 im = sud[r:r+28][:,c:c+28]
                 #pad = np.zeros(im.shape)
-                #pad[3:-3][:,3:-3] = sud[r+3:r+25][:,c+3:c+25]
+                centroid = sud[r+5:r+20][:,c+5:c+20]
+                #plt.imshow(centroid)
+                
+                    
+                
+                contours, hierarchy = cv2.findContours(im,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+                #cv2.drawContours(im, contours, -1, (0,255,0), 3)
+                
+                if sum(centroid.ravel()) == 0:
+                    print(im.shape)
+                    im = im * 0
+                else:
+                    t = im.copy()
+                    t2 = im.copy() * 0
+                    maxc = max(contours, key = cv2.contourArea)
+                    
+                    cv2.drawContours(t2, [maxc], -1, 255, 2)
+        
+                    
+                    
+                    cv2.fillPoly(t2, pts =[maxc], color=255)
+                    im = cv2.bitwise_and(t,t2)
+               
                 cells.append(im)
+                #plt.imshow(im)
+                #print(sud[r:r+28][:,c:c+28])
+               
+        
+        
         cells = np.array(cells)
 
         cells = cells.reshape((-1,28,28,1))
