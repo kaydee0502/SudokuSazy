ba9d08f61ba7071f62f67ad308727ef45a09e2ca
kaydee0502 <kshitijdhama513@gmail.com>
2021-04-03 10:47:45 +0530

    added about us

--- app.py
+++ app.py
@@ -38,6 +38,9 @@ def home():
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/results") 
 def res():
